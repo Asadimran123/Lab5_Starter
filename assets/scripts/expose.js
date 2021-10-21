3ffb73ddc868d2f3e7a69fdc717e7660134288bb
Asad Imran <m1imran@ucsd.edu>
2021-10-20 17:31:28 -0700

    image feature added

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,6 +2,14 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
-function init() {
-  // TODO
+function selectImage(){
+  var hornImage = document.getElementsByTagName('img')[0];
+  hornImage.setAttribute("src", "assets/images/" + event.target.value + ".svg");
 }
+
+function init() {
+  //control whcih horn image is displayed
+  var horn = document.getElementById("horn-select");
+  horn.addEventListener('click', selectImage);
+}
+
